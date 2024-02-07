aaa9f2b3b2dffe57de183679594b2fba163bb4ab
21toffy <oketofoke@gmail.com>
2024-02-07 21:23:36 +0100

    new

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../vendor/animate/animate.css';  // Import styles from styles1.css
+import '../css/style.css';  // Import styles from styles2.css
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,399 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import React, { useRef, useState, useEffect } from 'react';
 
+import {AnimatePresence, motion} from "framer-motion";
 export default function Home() {
+
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+  const [isVisible, setisVisible] = useState(true);
+
+
+  
+
+  const handleToggleClick = () => {
+    setIsOverlayOpen(!isOverlayOpen);
+  };
+
+
+  const slideRef = useRef(null);
+
+  const handleNextClick = () => {
+    const items = slideRef.current.querySelectorAll('.item');
+    slideRef.current.appendChild(items[0]);
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % items.length);
+  };
+  
+  const handlePrevClick = () => {
+    const items = slideRef.current.querySelectorAll('.item');
+    slideRef.current.prepend(items[items.length - 1]);
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
+  };
+  
+
+
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [containerHeight, setContainerHeight] = useState(0);
+
+
+useEffect(() => {
+  const handleResize = () => {
+    if (window) {
+      setContainerWidth(window.innerWidth);
+      setContainerHeight(window.innerHeight);
+    }
+  };
+
+  handleResize(); // Initial set based on window size
+  window.addEventListener('resize', handleResize);
+
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
+
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const images = [
+    'https://i.ibb.co/qCkd9jS/img1.jpg',
+    'https://i.ibb.co/jrRb11q/img2.jpg',
+    'https://i.ibb.co/NSwVv8D/img3.jpg',
+    'https://i.ibb.co/Bq4Q0M8/img4.jpg',
+    'https://i.ibb.co/jTQfmTq/img5.jpg',
+    'https://i.ibb.co/RNkk6L0/img6.jpg',
+  ];
+
+  useEffect(() => {
+    const handleResize = () => {
+      setContainerWidth(window.innerWidth);
+      setContainerHeight(window.innerHeight);
+    };
+  
+    window.addEventListener('resize', handleResize);
+  
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+  
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* Font Awesome CDN */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+      <div className="container"
+        style={{
+          backgroundImage: `url(${images[currentImageIndex]})`,
+          width: `${containerWidth}px`,
+          height: `${containerHeight}px`,
+        }}
+      >
+        <div className="slide" ref={slideRef}>
+          <div
+            className="item"
+            style={{
+              backgroundImage: "url(https://i.ibb.co/qCkd9jS/img1.jpg)",
+            }}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            <div className="content">
+              <div className="name">Switzerland</div>
+              <div className="des">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab,
+                eum!
+              </div>
+              <button>See More</button>
+            </div>
+          </div>
+          <div
+            className="item"
+            style={{
+              backgroundImage: "url(https://i.ibb.co/jrRb11q/img2.jpg)",
+            }}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className="content">
+              <div className="name">Finland</div>
+              <div className="des">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab,
+                eum!
+              </div>
+              <button>See More</button>
+            </div>
+          </div>
+          <div
+            className="item"
+            style={{
+              backgroundImage: "url(https://i.ibb.co/NSwVv8D/img3.jpg)",
+            }}
+          >
+            <div className="content">
+              <div className="name">Iceland</div>
+              <div className="des">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab,
+                eum!
+              </div>
+              <button>See More</button>
+            </div>
+          </div>
+          <div
+            className="item"
+            style={{
+              backgroundImage: "url(https://i.ibb.co/Bq4Q0M8/img4.jpg)",
+            }}
+          >
+            <div className="content">
+              <div className="name">Australia</div>
+              <div className="des">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab,
+                eum!
+              </div>
+              <button>See More</button>
+            </div>
+          </div>
+          <div
+            className="item"
+            style={{
+              backgroundImage: "url(https://i.ibb.co/jTQfmTq/img5.jpg)",
+            }}
+          >
+            <div className="content">
+              <div className="name">Netherlands</div>
+              <div className="des">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab,
+                eum!
+              </div>
+              <button>See More</button>
+            </div>
+          </div>
+          <div
+            className="item"
+            style={{
+              backgroundImage: "url(https://i.ibb.co/RNkk6L0/img6.jpg)",
+            }}
+          >
+            <div className="content">
+              <div className="name">Ireland</div>
+              <div className="des">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab,
+                eum!
+              </div>
+              <button>See More</button>
+            </div>
+          </div>
         </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+        <div className="button">
+          <button className="prev" onClick={handlePrevClick}>
+            <i className="fas fa-arrow-left"></i>
+          </button>
+          <button className="next" onClick={handleNextClick}>
+            <i className="fas fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
 
       <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
+        @media (max-width: 768px) {
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
+          body {
+            background: #eaeaea;
+            overflow: hidden;
+          }
+
+          .container {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 100%;
+            height: 100vh;
+            overflow: hidden;
+          }
+
+          .slide {
+            display: flex;
+            transition: transform 0.5s ease-in-out;
+          }
+
+          .item {
+            flex: 0 0 100%;
+            height: 100vh;
+            background-size: cover;
+          }
+
+          .button button {
+            display: block;
+          }
+
+          .button button {
+            width: 40px;
+            height: 35px;
+            border-radius: 8px;
+            border: none;
+            cursor: pointer;
+            margin: 0 5px;
+            border: 1px solid #000;
+            transition: 0.3s;
+          }
+
+          .button button:hover {
+            background: #ababab;
+            color: #fff;
+          }
         }
         * {
+          margin: 0;
+          padding: 0;
           box-sizing: border-box;
         }
+
+        body {
+          background: #eaeaea;
+          overflow: hidden;
+        }
+
+        .container {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          width: 1000px;
+          height: 600px;
+          background: #f5f5f5;
+          box-shadow: 0 30px 50px #dbdbdb;
+        }
+
+        .container .slide .item {
+          width: 200px;
+          height: 300px;
+          position: absolute;
+          top: 50%;
+          transform: translate(0, -50%);
+          border-radius: 20px;
+          box-shadow: 0 30px 50px #505050;
+          background-position: 50% 50%;
+          background-size: cover;
+          display: inline-block;
+          transition: 0.5s;
+        }
+
+        .slide .item:nth-child(1),
+        .slide .item:nth-child(2) {
+          top: 0;
+          left: 0;
+          transform: translate(0, 0);
+          border-radius: 0;
+          width: 100%;
+          height: 100%;
+        }
+
+        .slide .item:nth-child(3) {
+          left: 50%;
+        }
+        .slide .item:nth-child(4) {
+          left: calc(50% + 220px);
+        }
+        .slide .item:nth-child(5) {
+          left: calc(50% + 440px);
+        }
+
+        /* here n = 0, 1, 2, 3,... */
+        .slide .item:nth-child(n + 6) {
+          left: calc(50% + 660px);
+          opacity: 0;
+        }
+
+        .item .content {
+          position: absolute;
+          top: 50%;
+          left: 100px;
+          width: 300px;
+          text-align: left;
+          color: #eee;
+          transform: translate(0, -50%);
+          font-family: system-ui;
+          display: none;
+        }
+
+        .slide .item:nth-child(2) .content {
+          display: block;
+        }
+
+        .content .name {
+          font-size: 40px;
+          text-transform: uppercase;
+          font-weight: bold;
+          opacity: 0;
+          animation: animate 1s ease-in-out 1 forwards;
+        }
+
+        .content .des {
+          margin-top: 10px;
+          margin-bottom: 20px;
+          opacity: 0;
+          animation: animate 1s ease-in-out 0.3s 1 forwards;
+        }
+
+        .content button {
+          padding: 10px 20px;
+          border: none;
+          cursor: pointer;
+          opacity: 0;
+          animation: animate 1s ease-in-out 0.6s 1 forwards;
+        }
+
+        @keyframes animate {
+          from {
+            opacity: 0;
+            transform: translate(0, 100px);
+            filter: blur(33px);
+          }
+
+          to {
+            opacity: 1;
+            transform: translate(0);
+            filter: blur(0);
+          }
+        }
+
+        .button {
+          width: 100%;
+          text-align: center;
+          position: absolute;
+          bottom: 20px;
+        }
+
+        .button button {
+          width: 40px;
+          height: 35px;
+          border-radius: 8px;
+          border: none;
+          cursor: pointer;
+          margin: 0 5px;
+          border: 1px solid #000;
+          transition: 0.3s;
+        }
+
+        .button button:hover {
+          background: #ababab;
+          color: #fff;
+        }
       `}</style>
-    </div>
+    </>
   );
-}
+};
+
